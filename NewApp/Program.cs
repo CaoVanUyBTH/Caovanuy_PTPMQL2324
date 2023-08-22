5dc06e14a7e62542fdcc677b1aeb69c828070780
cao-uy <caovanuy00@gmail.com>
2023-08-22 11:16:02 +0700

    Phuong thuc co tham so

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,14 +3,12 @@ using NewApp.Models.Models;
 
 public class Program
 {
-    private static void Main (string[] args)
+    private static void Main(string[]args)
     {
-         Person ps1 = new Person();
-         Person ps2 = new Person();
-         ps1.FullName = "Nguyen Van A";
-         ps1.Address = "Ha Noi";
-         ps1.Age = 18;
-         ps1.Display();
-         ps2.Display();
+        //Khoi tao doi tuong ps tu class Person
+        Person ps = new Person();
+        string str = "Nguyen Van A";
+        int a = 22;
+        ps.Display2(str, a);
     }
 }
