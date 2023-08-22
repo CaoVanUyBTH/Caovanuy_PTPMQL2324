9ff5c680938e1ee0525f4a73e91352c7476074e6
cao-uy <caovanuy00@gmail.com>
2023-08-22 11:03:59 +0700

    Phuong thuc khoi tao trong C#

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using NewApp.Models.Models;
+
+public class Program
+{
+    private static void Main (string[] args)
+    {
+         Person ps1 = new Person();
+         Person ps2 = new Person();
+         ps1.FullName = "Nguyen Van A";
+         ps1.Address = "Ha Noi";
+         ps1.Age = 18;
+         ps1.Display();
+         ps2.Display();
+    }
+}
