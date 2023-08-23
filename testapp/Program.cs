19efc18318e7c2f2f48d7ffcb2d1197105841627
cao-uy <caovanuy00@gmail.com>
2023-08-23 09:48:55 +0700

    Tao doi tuong

---
+++ testapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using testapp.test;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        test NguyenVanA = new test();
+        test NguyenVanB = new test();
+        test NguyenVanC = new test();
+        test NguyenVanD = new test();
+        test NguyenVanE = new test();
+    }
+}
